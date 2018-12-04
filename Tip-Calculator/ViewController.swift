217da2c4564fdfbc86d071610dcc68e2b468d592
joypaul <jpaul3250@gmail.com>
2018-12-03 19:16:30 -0500

    Added the foundation of the pie chart

--- Tip-Calculator/ViewController.swift
+++ Tip-Calculator/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import PieCharts
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadInChart()
     }
-
+    
+  
+    @IBOutlet weak var chartView: PieChart!
+    
+    func loadInChart(){
+        
+        //adding a line text overlay
+        chartView.layers = [PieLineTextLayer()]
+        
+        //insert the values + color of the slice
+        chartView.models = [
+            PieSliceModel(value: 3.2121, color: UIColor.brown),
+            PieSliceModel(value: 3.323, color: UIColor.blue),
+            PieSliceModel(value: 1.1212, color: UIColor.green)
+        ]
+    }
+    
 
 }
 
